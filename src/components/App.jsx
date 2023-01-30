66a46c51a5b95180c59338c543f9448463505811
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-30 13:33:14 -0600

    passing props to Card component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,19 @@
 import "../styles/App.css";
+import Navbar from "./Navbar";
+import Card from "./Card";
+import cardData from "../data/data";
 
 const App = () => {
-  return <div className="App"></div>;
+  const data = cardData.map((info) => {
+    return <Card key={info.id} {...info} />;
+  });
+
+  return (
+    <div className="App">
+      <Navbar />
+      <section className="cards-container">{data}</section>
+    </div>
+  );
 };
 
 export default App;
