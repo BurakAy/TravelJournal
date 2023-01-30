0ca1c0800652f6e928ef50eb5a0e4b4b4ca2cfc7
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-30 13:30:46 -0600

    added custom color to tailwind

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "travel-red": "#F55A5A",
+      },
+    },
   },
   plugins: [],
 };
