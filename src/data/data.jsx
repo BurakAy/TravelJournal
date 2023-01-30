1da8fe3bfcccdaabde4ecb08cf8897b3350b23b7
Burak <14030652+BurakAy@users.noreply.github.com>
2023-01-30 13:31:10 -0600

    added data file

---
+++ src/data/data.jsx
@@ -0,0 +1,38 @@
+export default [
+  {
+    id: 1,
+    country: "Turkiye",
+    mapUrl: "https://goo.gl/maps/3UGkaTwu4RHJZCQEA",
+    imgUrl:
+      "https://images.unsplash.com/photo-1590533821454-ef0a004463dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80",
+    location: "Ephesus",
+    startDate: "20 Nov, 2022",
+    endDate: "28 Nov, 2022",
+    description:
+      "Ephesus was an ancient port city whose well-preserved ruins are in modern-day Turkey. Much of Ephesus’s ancient history is unrecorded and sketchy. What is known is that in the seventh century B.C., Ephesus fell under the rule of the Lydian Kings and became a thriving city where men and women enjoyed equal opportunities. It was also the birthplace of the renowned philosopher Heraclitus.",
+  },
+  {
+    id: 2,
+    country: "UAE",
+    mapUrl: "https://goo.gl/maps/iMUnJg6JYs457wvy7",
+    imgUrl:
+      "https://images.unsplash.com/photo-1582672060674-bc2bd808a8b5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+    location: "Burj Khalifa",
+    startDate: "10 Jan, 2018",
+    endDate: "15 Jan, 2018",
+    description:
+      "Not only is Burj Khalifa the world's tallest building but it has also broken two other impressive records: tallest structure, previously held by the KVLY-TV mast in Blanchard, North Dakota, and tallest free-standing structure, previously held by Toronto's CN Tower.",
+  },
+  {
+    id: 3,
+    country: "Canada",
+    mapUrl: "https://goo.gl/maps/w6BPKgCVbphA7ZkR8",
+    imgUrl:
+      "https://images.unsplash.com/photo-1625616257448-fb5a25943809?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1050&q=80",
+    location: "Casa Loma",
+    startDate: "05 Feb, 2002",
+    endDate: "11 Feb, 2022",
+    description:
+      "Casa Loma was first built in 1914 by financier Sir Henry Pellatt. The castle located in midtown Toronto, is now owned by the City of Toronto and is regarded as a treasured heritage landmark.",
+  },
+];
